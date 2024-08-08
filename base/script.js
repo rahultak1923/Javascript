7a03de3164443e6aa13e8b5a93991690067ac304
Rahul Tak <95410579+rahultak1923@users.noreply.github.com>
2024-08-08 17:01:42 +0530

    1_variable-datatypes

---
+++ base/script.js
@@ -0,0 +1,27 @@
+console.log("hello ");
+console.log(2+3);
+
+// variables 
+
+var a = "this is a variable ";
+console.log(a);
+
+let b = "this is a also variable ";
+console.log(b);
+
+const c = "this is a const ";
+console.log(c)
+
+// data types 
+
+// 1. number 
+
+let num = 3.42;
+let num2 = 10;
+console.log(num + num2);
+
+// 2. strings
+
+const firstName = 'rahul';
+const lastName = "tak";
+console.log(firstName+ " " + lastName);
